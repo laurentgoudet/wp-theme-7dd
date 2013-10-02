12003f29a35cf938104a50c532ee7cc350b0533a
Laurent Goudet <me@laurentgoudet.com>
2013-10-02 14:54:48 +1000

    Remove CC-BY image

--- footer.php
+++ footer.php
@@ -17,10 +17,6 @@
 
 			<div class="site-info">
 				<?php do_action( 'twentythirteen_credits' ); ?>
-		        <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/deed.en_US">
-		          <img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-sa/3.0/88x31.png" />
-		        </a>
-		        <br />
 		        <p id="cc-desc">
 		         All articles and photos of this blog are licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/deed.en_US">Creative Commons Attribution-ShareAlike 3.0 Unported License</a>.
 		        </p>
